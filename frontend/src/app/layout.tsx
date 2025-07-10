f58bc56b85ae848a4e866d50ccc7945b982f563c
Benjamin Okumu <okumub85@gmail.com>
2025-07-10 21:32:32 +0300

    update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
-import { SessionProvider } from 'next-auth/react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { ReactNode } from 'react';
+import SessionWrapper from '@/components/SessionWrapper';
 
 export const metadata = {
   title: 'Benjamin Okumu Portfolio',
@@ -13,11 +13,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
-        <SessionProvider>
+        <SessionWrapper>
           <Header />
           <main className="flex-grow">{children}</main>
           <Footer />
-        </SessionProvider>
+        </SessionWrapper>
       </body>
     </html>
   );
